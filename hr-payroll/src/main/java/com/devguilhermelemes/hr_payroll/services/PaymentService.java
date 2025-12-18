c5a42ca605a8c971448feeeec5cec52ba22d942c
devGuilhermeLemes <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>
2025-12-18 14:04:05 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/devguilhermelemes/hr_payroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.devguilhermelemes.hr_payroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devguilhermelemes.hr_payroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days){
+        return new Payment("Bob", 200.00, days);
+    }
+
+}
