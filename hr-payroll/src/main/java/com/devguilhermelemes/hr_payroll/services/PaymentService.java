89e794cfc4c22ebb6efe7f747d96d63565a8e0da
devGuilhermeLemes <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>
2025-12-18 14:32:30 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/devguilhermelemes/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devguilhermelemes/hr_payroll/services/PaymentService.java
@@ -1,14 +1,30 @@
 package com.devguilhermelemes.hr_payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.devguilhermelemes.hr_payroll.entities.Payment;
+import com.devguilhermelemes.hr_payroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.00, days);
-    }
+    @Value("${hr-worker.host}")
+    private String workerHost;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(long workerId, int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", "" + String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+    }
 }
