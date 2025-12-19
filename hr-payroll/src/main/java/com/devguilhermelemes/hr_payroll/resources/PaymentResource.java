af7c022a2198fd5b0d497ff2e582dbbf1ce1bff1
devGuilhermeLemes <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>
2025-12-19 16:38:13 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/devguilhermelemes/hr_payroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devguilhermelemes/hr_payroll/resources/PaymentResource.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devguilhermelemes.hr_payroll.entities.Payment;
 import com.devguilhermelemes.hr_payroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -18,10 +19,16 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
-    
+
+    public ResponseEntity<Payment> getPaymentAlternative(long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
+
 }
