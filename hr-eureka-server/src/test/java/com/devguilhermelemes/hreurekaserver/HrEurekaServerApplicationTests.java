f6cc345f48084157bfc7f4069ff7f96a6b3addea
devGuilhermeLemes <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>
2025-12-19 13:42:22 -0300

    Configuring Eureka server

--- hr-eureka-server/src/test/java/com/devguilhermelemes/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/devguilhermelemes/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devguilhermelemes.hr_eureka_server;
+package com.devguilhermelemes.hreurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
