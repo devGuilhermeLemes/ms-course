877451843bb5f6f3e1a4b70698c9d0e0b82152d1
devGuilhermeLemes <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>
2025-12-30 17:01:54 -0300

    Configuring hr-user project

--- hr-user/src/main/java/com/devguilhermelemes/hr_user/HrUserApplication.java
+++ hr-user/src/main/java/com/devguilhermelemes/hr_user/HrUserApplication.java
@@ -3,11 +3,13 @@ package com.devguilhermelemes.hr_user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+		System.out.println("ok");
 	}
 
 }
