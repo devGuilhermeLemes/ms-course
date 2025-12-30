bd67382c8cb443ba06bff387172ff9d433ea0d64
devGuilhermeLemes <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>
2025-12-30 15:10:41 -0300

    Configuring hr-config-server project

--- hr-config-server/src/main/java/com/devguilhermelemes/hr_config_server/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devguilhermelemes/hr_config_server/HrConfigServerApplication.java
@@ -2,12 +2,15 @@ package com.devguilhermelemes.hr_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+		System.out.println("ok");
 	}
 
 }
