fe872070562ab0802725b3d6d33634f42bad15df
devGuilhermeLemes <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>
2026-01-03 18:33:59 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/devguilhermelemes/hr_oauth/entities/Role.java
@@ -0,0 +1,57 @@
+package com.devguilhermelemes.hr_oauth.entities;
+
+import java.io.Serializable;
+
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getroleName() {
+        return roleName;
+    }
+
+    public void setroleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+}
